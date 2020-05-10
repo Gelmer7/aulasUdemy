a0c6adef3c6188736105aa38eb8f7de3eee3a63e
GELMER <>
2020-05-09 23:11:04 -0300

    rutas funcionando, colocando contenido

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CadastrarComponent } from './components/cadastrar/cadastrar.component';
+import { CursosComponent } from './components/cursos/cursos.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'home', component:HomeComponent },
+  { path:'cursos', component:CursosComponent },
+  { path:'cadastrar', component:CadastrarComponent },
+  { path:'about', component:AboutComponent },
+  { path:'**', pathMatch:'full', redirectTo:'home' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CursosComponent } from './components/cursos/cursos.component';
+import { HomeComponent } from './components/home/home.component';
+import { CadastrarComponent } from './components/cadastrar/cadastrar.component';
+import { AboutComponent } from './components/about/about.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CursosComponent,
+    HomeComponent,
+    CadastrarComponent,
+    AboutComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
